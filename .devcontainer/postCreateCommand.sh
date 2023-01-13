5e62601a3f4ee653e22a2959d1e23b4038995301
msuthar09 <maharshisuthar09@gmail.com>
2023-01-13 18:28:10 +0000

    Initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+# Remove Windows EndOfLine \r to run in unix env
+# sed -i 's/\r$//' postCreateCommand.sh
+
+# Initialize all Git submodules
+git config --global --replace-all "safe.directory" "*"
+# git submodule update --init
+
+# Setup Python repo
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade --no-warn-script-location --requirement requirements.txt
+python3 -m pip install --upgrade --no-warn-script-location --requirement requirements/requirements.build.txt
+python3 -m pip install --upgrade --no-warn-script-location --requirement requirements/requirements.dev.txt
+python3 -m pip install --upgrade --no-warn-script-location --requirement requirements/requirements.docs.txt
+python3 -m pip install --upgrade --no-warn-script-location --requirement requirements/requirements.tests.txt
+sh -c "pre-commit install && pre-commit run --all-files"
